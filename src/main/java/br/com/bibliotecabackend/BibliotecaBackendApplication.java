cc7754e293ab5f93a478ebb18fc5b10b77661824
Lucas Valentim <lucas.valentim.costa@gmail.com>
2021-06-24 17:03:12 -0300

    nome da classse principal refatorada

--- src/main/java/br/com/bibliotecabackend/BibliotecaBackendApplication.java
+++ src/main/java/br/com/bibliotecabackend/BibliotecaBackendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlibliotecaBackendApplication {
+public class BibliotecaBackendApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BlibliotecaBackendApplication.class, args);
+		SpringApplication.run(BibliotecaBackendApplication.class, args);
 	}
 
 }
